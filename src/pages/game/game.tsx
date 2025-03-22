4470089d8085d91b2eaf531f3e27ba1015dd43f2
patitow <msoliver2002@gmail.com>
2025-03-22 19:28:29 -0300

    Initial test for 3D character movement and setup working as intended

--- src/pages/game/game.tsx
+++ src/pages/game/game.tsx
@@ -1,18 +1,32 @@
 import './game.css'
 import { Canvas } from '@react-three/fiber'
+import Experience from './elements/experience'
+import { KeyboardControls } from '@react-three/drei'
+
+const keyboardMap = [
+  { name: 'forward', keys: ['ArrowUp', 'KeyW'] },
+  { name: 'backward', keys: ['ArrowDown', 'KeyS'] },
+  { name: 'left', keys: ['ArrowLeft', 'KeyA'] },
+  { name: 'right', keys: ['ArrowRight', 'KeyD'] },
+  { name: 'run', keys: ['Shift'] },
+]
 
 function PolimapGame() {
   return (
-    <>
-      <Canvas>
-        <mesh>
-          <boxGeometry args={[2, 2, 2]} />
-          <meshPhongMaterial />
-        </mesh>
-        <ambientLight intensity={0.1} />
-        <directionalLight position={[0, 0, 5]} color="white" />
+    <KeyboardControls map={keyboardMap}>
+      <Canvas
+        style={{
+          width: '100dvw',
+          height: '100dvh',
+          touchAction: 'none',
+        }}
+        shadows
+        camera={{ position: [3, 3, 3], near: 0.1, fov: 40 }}
+      >
+        <color attach="background" args={['#ececec']} />
+        <Experience />
       </Canvas>
-    </>
+    </KeyboardControls>
   )
 }
 
