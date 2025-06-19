47b19e8e2c04043c2b3831440e74509ae474d78e
patitow <msoliver2002@gmail.com>
2025-06-19 20:10:01 -0300

    other changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
+import vercel from 'vite-plugin-vercel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [vercel(), react(), tailwindcss()],
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
