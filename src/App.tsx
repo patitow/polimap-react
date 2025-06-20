35a6046ba3f3f676eb3a2125a2d6ca2589924b73
patitow <msoliver2002@gmail.com>
2025-06-19 21:34:29 -0300

    man i hate my life

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,11 @@ import { ThemeProvider } from './providers/themeProvider'
 import Acesso from './pages/acesso/acesso'
 import Sobre from './pages/sobre/sobre'
 import Tutorial from './pages/tutorial/tutorial'
-import Navbar from './components/nav/navbar'
 
 function App() {
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/acessar" element={<Acesso />} />
